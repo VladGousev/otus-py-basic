349d59ee88b99cf83ff4a8b50e52f4e563e2f710
Vladislav Gusev <VladGousev@yandex.ru>
2024-03-24 19:10:32 +0300

    feat(homework_05): create / and /about/

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,20 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return "Hello, World!"
+
+
+@app.get("/about/")
+def about():
+    return "Homework #5 from OTUS PythonBasic"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
