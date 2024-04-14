0c9f99acfe68821238b1775b7a5cc7f02a2b7c9e
Vladislav Gusev <VladGousev@yandex.ru>
2024-04-14 23:57:37 +0300

    feat(homework_06): add views, templates and crud methods for detail view and delete user

--- homework_06/src/views/users/crud.py
+++ homework_06/src/views/users/crud.py
@@ -4,5 +4,17 @@ from models.db import db
 from models.user import User
 
 
+def create_user(name: str) -> User:
+    user = User(name=name)
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+
 def get_users() -> list[User]:
     return list(db.session.scalars(select(User)).all())
+
+
+def delete_user(user: User) -> None:
+    db.session.delete(user)
+    db.session.commit()
--- homework_06/src/views/users/views.py
+++ homework_06/src/views/users/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.exceptions import BadRequest
 
+from models.user import User
 from views.users import crud
 
 users_app = Blueprint(
@@ -16,5 +18,67 @@ users_app = Blueprint(
 def get_users():
     return render_template(
         "users/list.html",
-        products=crud.get_users(),
+        users=crud.get_users(),
     )
+
+
+@users_app.route(
+    "/add/",
+    endpoint="add",
+    methods=["GET", "POST"],
+)
+def create_user():
+    if request.method == "GET":
+        return render_template("users/add.html")
+
+    user_name = request.form.get("user-name", "")
+    user_name = user_name.strip()
+    if not user_name:
+        raise BadRequest("user-name is required!")
+
+    user = crud.create_user(
+        name=user_name,
+    )
+
+    url = url_for(
+        "users_app.details",
+        user_id=user.id,
+    )
+    return redirect(url)
+
+
+@users_app.get(
+    "/<int:user_id>/",
+    endpoint="detail",
+)
+def get_user_detail(user_id: int):
+    user: User = User.query.get_or_404(
+        user_id,
+        description=f"User #{user_id} not found!",
+    )
+    return render_template(
+        "users/detail.html",
+        user=user,
+    )
+
+
+@users_app.route(
+    "/<int:user_id>/confirm-delete/",
+    endpoint="delete",
+    methods=["GET", "POST"],
+)
+def delete_user(user_id: int):
+    user: User = User.query.get_or_404(
+        user_id,
+        description=f"User #{user_id} not found!",
+    )
+    if request.method == "GET":
+        return render_template(
+            "users/delete.html",
+            user=user,
+        )
+
+    crud.delete_user(user)
+
+    url = url_for("users_app.list")
+    return redirect(url)
